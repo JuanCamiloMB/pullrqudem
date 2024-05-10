2c0a9291a9f19f20452865d5039b7582f0040378
Juan Camilo Manjarrés Baena <63174756+JuanCamiloMB@users.noreply.github.com>
2024-05-10 16:50:12 -0500

    Update suma.py

--- suma.py
+++ suma.py
@@ -1,4 +1,15 @@
-a = input("entre el primer numero: ")
-b = input("entre el segundo numero: ")
-suma = a + b
-print("La suma de los dos números es: ", suma)
+import sys
+
+def sum_numbers(num1, num2):
+    return num1 + num2
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python program.py <num1> <num2>")
+        sys.exit(1)
+
+    num1 = float(sys.argv[1])
+    num2 = float(sys.argv[2])
+
+    result = sum_numbers(num1, num2)
+    print("Sum:", result)
